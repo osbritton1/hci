d3c8d28e7231d0b5236b87ccce6ddd00181b39ee
osbritton1 <obritton2022@gmail.com>
2026-02-09 12:21:55 -0500

    Wrote rank table generation and ranking code

---
+++ hci/lib/hci.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MAX_PRECOMP 64
+
+bool get_consecutive_combs(uint64_t *comb_list, int norb, int nocc);
+void get_rank_table(uint64_t *table, int norb, int nocc);
+uint64_t rank(int *occ_list, uint64_t *rank_table, int norb, int nocc);
+int unrank(int rank, int norb, int nocc);
