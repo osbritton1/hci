Last commit before adopting great refactoring; fixed bug in magnitude calculation for double excitations

hci/.ipynb_checkpoints/hci_uhf-checkpoint.py
@@ -39,26 +39,86 @@ def kernel_new(myhci, hcore, eri_ao, mo, norb, nelec, add_thresh, ci0=None, tol=
     # Build ranking tables and excitation lists
     config_info = ConfigInfo(norb, nelec)
     excitation_entries = ExcitationEntries(eri_mo, config_info)
+    excitation_entries.sort_desc_by_mag()
 
     #Initialize HCI vector if initial guess not provided
     if ci0 is None:
         ranks = np.zeros(1, dtype=hcilib.rank_entry)
         hf_gnd = HCIVector.as_HCIVector(np.array([1.0], dtype=np.float64), ranks)
         ci0 = [hf_gnd]
-        ranks, coeffs = myhci.enlarge_space_new(ci0, add_thresh, config_info, excitation_entries, h1e, eri_mo)
+        ci0 = myhci.enlarge_space_new(ci0, add_thresh, config_info, excitation_entries, h1e, eri_mo)
+        ranks = ci0[0].ranks
+
+    # Define Hamiltonian-vector contraction and preconditioner for use in Davidson algorithm
+    def hop(c):
+        hc = hcilib.contract_hamiltonian_hcivec_slow_new(HCIVector.as_HCIVector(c, ranks), hdiag, config_info, h1e, eri_mo) 
+        return hc
+    precond = lambda x, e, *args: x/(hdiag-e+myhci.level_shift)
+
+    # Set convergence parameters
+    max_len = config_info.combmax_a*config_info.combmax_b
+    e_last = 0
+    float_tol = myhci.start_tol
+    tol_decay_rate = myhci.tol_decay_rate
+
+    # Begin HCI loop
+    for icycle in range(norb):
+        float_tol = max(float_tol*tol_decay_rate, tol*1e2)
+        log.debug('cycle %d  ci.shape %s  float_tol %g',
+                  icycle, (len(ranks),), float_tol)
+        hdiag = myhci.make_hdiag_slow_new(ci0[0], config_info, h1e, eri_mo)
+        e, ci0 = pyscflib.davidson(hop, ci0, precond, tol=float_tol, lindep=lindep,
+                                   max_cycle=max_cycle, max_space=max_space, nroots=nroots,
+                                   max_memory=max_memory, verbose=log, **kwargs)
+        if nroots > 1:
+            ci0 = [HCIVector.as_HCIVector(c, ranks) for c in ci0]
+            de, e_last = min(e)-e_last, min(e)
+            print('cycle %d  E = %s  dE = %.8g' % (icycle, e+ecore, de))
+            log.info('cycle %d  E = %s  dE = %.8g', icycle, e+ecore, de)
+        else:
+            ci0 = [HCIVector.as_HCIVector(ci0, ranks)]
+            de, e_last = e-e_last, e
+            print('cycle %d  E = %s  dE = %.8g' % (icycle, e+ecore, de))
+            log.info('cycle %d  E = %.15g  dE = %.8g', icycle, e+ecore, de)
+        
+        if len(ranks) == max_len or abs(de) < tol*1e3:
+            break
+
+        old_length = float(len(ci0[0]))
+        ci0 = myhci.enlarge_space_new(ci0, add_thresh, config_info, excitation_entries, h1e, eri_mo)
+        ranks = ci0[0].ranks
+        
+        new_length = float(len(ci0[0]))
+        if new_length/old_length < 1.01:
+            break
+
+    log.debug('Extra CI in selected space %s', (len(ci0[0]),))
+    hdiag = myhci.make_hdiag_slow_new(ci0[0], config_info, h1e, eri_mo)
+    e, c = pyscflib.davidson(hop, ci0, precond, tol=float_tol, lindep=lindep,
+                             max_cycle=max_cycle, max_space=max_space, nroots=nroots,
+                             max_memory=max_memory, verbose=log, **kwargs)
+    if nroots > 1:
+        for i, ei in enumerate(e+ecore):
+            print('Selected CI state %d  E = %.15g' % (i, ei))
+            log.info('Selected CI state %d  E = %.15g', i, ei)
+        return e+ecore, [HCIVector.as_HCIVector(ci, ranks) for ci in c]
     else:
-        ranks, coeffs = ci0
+        print('Selected CI  E = %.15g' % (e+ecore))
+        log.info('Selected CI  E = %.15g', e+ecore)
+        return e+ecore, HCIVector.as_HCIVector(c, ranks)
 
 def enlarge_space_new(myhci, hcivecs, add_thresh, config_info, excitation_entries, h1e, eri_mo):
     add_list_doubles, nadd_doubles = hcilib.enlarge_space_doubles_new(hcivecs[0], add_thresh, config_info, excitation_entries)
+    print(add_list_doubles[:nadd_doubles])
     add_list_singles, nadd_singles = hcilib.enlarge_space_singles_new(hcivecs[0], add_thresh, config_info, h1e, eri_mo)
+    print(add_list_singles[:nadd_singles])
     add_list = np.unique(np.concatenate([add_list_doubles[:nadd_doubles], add_list_singles[:nadd_singles]]))
     for exc_vec in hcivecs[1:]:
         add_list_doubles_exc, nadd_doubles_exc = hcilib.enlarge_space_doubles_new(exc_vec, add_thresh, config_info, excitation_entries)
         add_list_singles_exc, nadd_singles_exc = hcilib.enlarge_space_singles_new(exc_vec, add_thresh, config_info, h1e, eri_mo)
         add_list = np.unique(np.concatenate([add_list, add_list_doubles_exc[:nadd_doubles_exc], add_list_singles_exc[:nadd_singles_exc]]))
     
-    ndets_old = len(ranks)
+    ndets_old = len(hcivecs[0].ranks)
     total_ranks = np.concatenate([hcivecs[0].ranks, add_list])
     ranks_new, unique_rows = np.unique(total_ranks, return_index=True)
     ndets_new = len(ranks_new)
@@ -200,10 +260,12 @@ def enlarge_space(myhci, ranks, coeffs, norb, nelec_a, nelec_b, add_thresh,
                                                                   config_table_a, config_table_b, exc_table_4o, exc_table_2o, 
                                                                   doubles_aa, doubles_bb, mixed_ab,
                                                                   max_mag_aa, max_mag_bb, max_mag_ab)
+    print(add_list_doubles[:nadd_doubles])
     add_list_singles, nadd_singles = hcilib.enlarge_space_singles(ranks, coeffs[0], norb, nelec_a, nelec_b, add_thresh,
                                                                   config_table_a, config_table_a_complement,
                                                                   config_table_b, config_table_b_complement,
                                                                   h1e_aa, h1e_bb, eri_aaaa_s8, eri_bbbb_s8, eri_aabb_s4)
+    print(add_list_singles[:nadd_singles])
     add_list = np.unique(np.concatenate([add_list_doubles[:nadd_doubles], add_list_singles[:nadd_singles]]), axis=0)
     
     for exc_coeff in coeffs[1:]:
@@ -253,7 +315,10 @@ This is an inefficient dialect of heat-bath CI written as a senior thesis projec
 For efficient heat-bath CI programs, it is recommended to use the Dice program (https://github.com/sanshar/Dice.git).''')
         
     make_hdiag_slow = staticmethod(hcilib.make_hdiag_slow)
+    make_hdiag_slow_new = staticmethod(hcilib.make_hdiag_slow_new)
     enlarge_space = enlarge_space
+    enlarge_space_new = enlarge_space_new
     kernel = kernel
+    kernel_new = kernel_new
 
 HCI = HeatBathCI

hci/lib/hci_utils.py
@@ -6,11 +6,12 @@ import math
 rank_entry = np.dtype([('arank', np.uint64), ('brank', np.uint64)])
 double_excitation_entry = np.dtype([('rank', np.uint64), ('ijkl', np.double), ('iljk', np.double)])
 mixed_excitation_entry = np.dtype([('rank', np.uint64), ('ijkl', np.double)])
-# hci_entry = np.dtype([('arank', np.uint64), ('brank', np.uint64), ('coeff', np.double)])
 
 libhci = load_library("libhci")
 libhci.get_matrix_element_by_rank.restype = ct.c_double
 libhci.get_matrix_element_by_rank_test_storage.restype = ct.c_double
+libhci.get_matrix_element_by_rank_new.restype = ct.c_double
+libhci.get_matrix_element_by_rank_test_storage_new.restype = ct.c_double
 
 class CoreHamiltonian:
     def __init__(self, h1e_mo_aa, h1e_mo_bb):
@@ -58,19 +59,19 @@ class ConfigInfo:
         self.norb = norb
         self.nelec_a = nelec_a
         self.nelec_b = nelec_b
-        self.mixed_ncols = math.comb(nelec_a+1, 2)
+        self.mixed_ncols = math.comb(norb+1, 2)
         self.ptrs = self.ConfigInfoPtrs(self.config_table_a.ctypes.data_as(ct.c_void_p),
-                                          self.config_table_a_complement.ctypes.data_as(ct.c_void_p),
-                                          ct.c_uint64(self.combmax_a),
-                                          self.config_table_b.ctypes.data_as(ct.c_void_p),
-                                          self.config_table_b_complement.ctypes.data_as(ct.c_void_p),
-                                          ct.c_uint64(self.combmax_b),
-                                          self.exc_table_4o.ctypes.data_as(ct.c_void_p),
-                                          self.exc_table_2o.ctypes.data_as(ct.c_void_p),
-                                          ct.c_size_t(self.norb),
-                                          ct.c_size_t(self.nelec_a),
-                                          ct.c_size_t(self.nelec_b),
-                                          ct.c_uint64(self.mixed_ncols))
+                                        self.config_table_a_complement.ctypes.data_as(ct.c_void_p),
+                                        ct.c_uint64(self.combmax_a),
+                                        self.config_table_b.ctypes.data_as(ct.c_void_p),
+                                        self.config_table_b_complement.ctypes.data_as(ct.c_void_p),
+                                        ct.c_uint64(self.combmax_b),
+                                        self.exc_table_4o.ctypes.data_as(ct.c_void_p),
+                                        self.exc_table_2o.ctypes.data_as(ct.c_void_p),
+                                        ct.c_size_t(self.norb),
+                                        ct.c_size_t(self.nelec_a),
+                                        ct.c_size_t(self.nelec_b),
+                                        ct.c_uint64(self.mixed_ncols))
         self._as_parameter_ = ct.byref(self.ptrs)
 
     class ConfigInfoPtrs(ct.Structure):
@@ -88,14 +89,28 @@ class ConfigInfo:
                     ('mixed_ncols', ct.c_uint64)]
 
 # Create double excitation storage for aa and bb type excitations
-def get_stored_double_exc(eri_mo_xxxx_s8, config_info):
+def get_stored_double_exc(eri_mo_xxxx_s8, exc_table_4o, norb):
+    doubles = np.empty(math.comb(norb, 4), dtype=double_excitation_entry)
+    libhci.load_doubles_from_eri(doubles.ctypes, eri_mo_xxxx_s8.ctypes, 
+                                 exc_table_4o.ctypes, ct.c_size_t(norb))
+    return doubles
+
+# Create double excitation storage for aa and bb type excitations
+def get_stored_double_exc_new(eri_mo_xxxx_s8, config_info):
     doubles = np.empty(math.comb(config_info.norb, 4), dtype=double_excitation_entry)
     libhci.load_doubles_from_eri(doubles.ctypes, eri_mo_xxxx_s8.ctypes, 
                                  config_info.exc_table_4o.ctypes, ct.c_size_t(config_info.norb))
     return doubles
 
 # Create double excitation storage for ab type excitations
-def get_stored_mixed_exc(eri_mo_aabb_s4, config_info):
+def get_stored_mixed_exc(eri_mo_aabb_s4, exc_table_2o, norb):
+    mixed = np.empty(math.comb(norb, 2)**2, dtype=mixed_excitation_entry)
+    libhci.load_mixed_from_eri(mixed.ctypes, eri_mo_aabb_s4.ctypes,
+                               exc_table_2o.ctypes, ct.c_size_t(norb))
+    return mixed
+
+# Create double excitation storage for ab type excitations
+def get_stored_mixed_exc_new(eri_mo_aabb_s4, config_info):
     mixed = np.empty(math.comb(config_info.norb, 2)**2, dtype=mixed_excitation_entry)
     libhci.load_mixed_from_eri(mixed.ctypes, eri_mo_aabb_s4.ctypes,
                                config_info.exc_table_2o.ctypes, ct.c_size_t(config_info.norb))
@@ -109,13 +124,13 @@ def get_max_magnitudes(doubles):
 
 class ExcitationEntries:
     def __init__(self, eri_mo, config_info):
-        self.doubles_aa = get_stored_double_exc(eri_mo.eri_mo_aaaa_s8, config_info)
+        self.doubles_aa = get_stored_double_exc_new(eri_mo.eri_mo_aaaa_s8, config_info)
         self.max_mag_aa = get_max_magnitudes(self.doubles_aa)
         self.ndoubles_aa = len(self.doubles_aa)
-        self.doubles_bb = get_stored_double_exc(eri_mo.eri_mo_bbbb_s8, config_info)
+        self.doubles_bb = get_stored_double_exc_new(eri_mo.eri_mo_bbbb_s8, config_info)
         self.max_mag_bb = get_max_magnitudes(self.doubles_bb)
         self.ndoubles_bb = len(self.doubles_bb)
-        self.mixed_ab = get_stored_mixed_exc(eri_mo.eri_mo_aabb_s4, config_info)
+        self.mixed_ab = get_stored_mixed_exc_new(eri_mo.eri_mo_aabb_s4, config_info)
         self.max_mag_ab = np.abs(self.mixed_ab['ijkl'])
         self.nmixed_ab = len(self.mixed_ab)
         self.ptrs = self.ExcitationEntriesPtrs(self.doubles_aa.ctypes.data_as(ct.c_void_p),
@@ -150,6 +165,16 @@ class ExcitationEntries:
         sorted_indices = np.argsort(self.max_mag_ab)[::-1]
         self.mixed_ab = self.mixed_ab[sorted_indices]
         self.max_mag_ab = self.max_mag_ab[sorted_indices]
+        self.ptrs = self.ExcitationEntriesPtrs(self.doubles_aa.ctypes.data_as(ct.c_void_p),
+                                               self.max_mag_aa.ctypes.data_as(ct.c_void_p),
+                                               ct.c_size_t(self.ndoubles_aa),
+                                               self.doubles_bb.ctypes.data_as(ct.c_void_p),
+                                               self.max_mag_bb.ctypes.data_as(ct.c_void_p),
+                                               ct.c_size_t(self.ndoubles_bb),
+                                               self.mixed_ab.ctypes.data_as(ct.c_void_p),
+                                               self.max_mag_ab.ctypes.data_as(ct.c_void_p),
+                                               ct.c_size_t(self.nmixed_ab))
+        self._as_parameter_ = ct.byref(self.ptrs)
 
 class HCIVector(np.ndarray):
     '''An 2D np array for HCI coefficients'''
@@ -182,10 +207,19 @@ class HCIVector(np.ndarray):
 
     @property
     def _as_parameter_(self):
-        ptr = self.HCIVectorPtrs(self.ranks.ctypes.data_as(ct.c_void_p),
+        ptrs = self.HCIVectorPtrs(self.ranks.ctypes.data_as(ct.c_void_p),
                                  self.ctypes.data_as(ct.c_void_p),
                                  ct.c_size_t(len(self)))
-        return ct.byref(ptr)
+        return ct.byref(ptrs)
+
+class Rank(ct.Structure):
+    _fields_ = [('arank', ct.c_uint64),
+                ('brank', ct.c_uint64)]
+    
+    @classmethod
+    def from_rank_entry(cls, rank_entry):
+        return cls(rank_entry['arank'], rank_entry['brank'])
+
 
 def enlarge_space_doubles_new(hcivec, add_thresh, config_info, excitation_entries):
     norb = config_info.norb
@@ -207,6 +241,22 @@ def enlarge_space_singles_new(hcivec, add_thresh, config_info, h1e, eri_mo):
     add_list = np.empty(len(hcivec.ranks)*(nexc_a+nexc_b), dtype=rank_entry)
     nadd = libhci.enlarge_space_singles_new(hcivec, add_list.ctypes, ct.c_double(add_thresh), config_info, h1e, eri_mo)
     return add_list, nadd
+
+def get_matrix_element_by_rank_new(rank1, rank2, config_info, h1e, eri_mo):
+    return libhci.get_matrix_element_by_rank_new(rank1, rank2, config_info, h1e, eri_mo)
+    
+def get_matrix_element_by_rank_test_storage_new(rank1, rank2, config_info, excitation_entries, h1e, eri_mo):
+    return libhci.get_matrix_element_by_rank_test_storage_new(rank1, rank2, config_info, excitation_entries, h1e, eri_mo)
+
+def make_hdiag_slow_new(hcivec, config_info, h1e, eri_mo):
+    hdiag = np.empty(len(hcivec.ranks), dtype=np.double)
+    libhci.make_hdiag_slow_new(hcivec, hdiag.ctypes, config_info, h1e, eri_mo)
+    return hdiag
+
+def contract_hamiltonian_hcivec_slow_new(hcivec_old, hdiag, config_info, h1e, eri_mo):
+    coeffs_new = np.empty(len(hcivec_old), dtype=np.float64)
+    libhci.contract_hamiltonian_hcivec_slow_new(hcivec_old, coeffs_new.ctypes, hdiag.ctypes, config_info, h1e, eri_mo)
+    return coeffs_new
 
 # OLD SPAGHETTI
 # OLD SPAGHETTI