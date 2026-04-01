e59c65c8be4591ae1cbcd37302385a6de00dcd92
osbritton1 <obritton2022@gmail.com>
2026-04-01 18:34:41 -0400

    Finishing previous commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
 import subprocess
 
 subprocess.call("cd ..; doxygen", shell=True)
 
+sys.path.insert(0, os.path.abspath('../../'))
 
 project = 'HCI for PySCF'
 copyright = '2026, Simon Britton'
---
+++ docs/source/hci.rst
@@ -0,0 +1,28 @@
+hci package
+===========
+
+Subpackages
+-----------
+
+.. toctree::
+
+   hci.lib
+
+Submodules
+----------
+
+hci.hci\_uhf module
+-------------------
+
+.. automodule:: hci.hci_uhf
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: hci
+   :members:
+   :show-inheritance:
+   :undoc-members:
