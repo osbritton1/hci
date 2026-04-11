Found silent logical error in get_double_exc_value_from_store; fixed build settings to work properly when pip is used without -e flag

hci/lib/hci_contract.py
@@ -26,7 +26,7 @@ def get_matrix_element_by_rank(rank1: Rank, rank2: Rank, config_info: ConfigInfo
     Returns:
         float: The matrix element between the configurations specified
     """    
-    return libhci.get_matrix_element_by_rank(rank1, rank2, config_info, h1e, eri_mo).value
+    return libhci.get_matrix_element_by_rank(rank1, rank2, config_info, h1e, eri_mo)
     
 def get_matrix_element_by_rank_test_storage(rank1: Rank, rank2: Rank, config_info: ConfigInfo, 
                                             exc_entries: ExcEntries, h1e: HCore, eri_mo: ERITensor) -> float:
@@ -49,7 +49,7 @@ def get_matrix_element_by_rank_test_storage(rank1: Rank, rank2: Rank, config_inf
     """
     if exc_entries.sort_type != ExcEntries.SortType.INC_BY_RANK:
         raise ExcEntries.SortTypeError("Calling this method requires the excitation entries to be sorted increasing by rank.")
-    return libhci.get_matrix_element_by_rank_test_storage(rank1, rank2, config_info, exc_entries, h1e, eri_mo).value
+    return libhci.get_matrix_element_by_rank_test_storage(rank1, rank2, config_info, exc_entries, h1e, eri_mo)
 
 def make_hdiag_slow(hcivec: HCIVec, config_info: ConfigInfo, h1e: HCore, eri_mo: ERITensor) -> npt.NDArray[np.float64]:
     """Assembles all diagonal matrix elements for configurations contained in **hcivec**; used for the Davidson preconditioner.

hci/lib/hci_rank.py
@@ -96,7 +96,7 @@ class ConfigInfo:
         Returns:
             int: The rank of the specified :math:`\alpha` occupancy list
         """        
-        return libhci.rank_occ_a(occ_list_a.ctypes, self).value
+        return libhci.rank_occ_a(occ_list_a.ctypes, self)
     
     def unrank_occ_a(self, arank: int) -> npt.NDArray[np.uintp]:
         r"""Determines which orbitals are in an :math:`\alpha` occupancy list of given rank.
@@ -134,7 +134,7 @@ class ConfigInfo:
         Returns:
             The rank of the specified :math:`\beta` occupancy list
         """        
-        return libhci.rank_occ_b(occ_list_b.ctypes, self).value
+        return libhci.rank_occ_b(occ_list_b.ctypes, self)
     
     def unrank_occ_b(self, brank: int) -> npt.NDArray[np.uintp]:
         r"""Determines which orbitals are in an :math:`\beta` occupancy list of given rank.
@@ -171,7 +171,7 @@ class ConfigInfo:
         Returns:
             The rank of the specified double excitation
         """        
-        return libhci.rank_double_exc(exc_list.ctypes, self).value
+        return libhci.rank_double_exc(exc_list.ctypes, self)
     
     def unrank_double_exc(self, exc_rank: int) -> npt.NDArray[np.uintp]:
         """Determines the four orbitals involved in a double excitation of given rank.
@@ -196,7 +196,7 @@ class ConfigInfo:
         Returns:
             The rank of the specified mixed excitation
         """        
-        return libhci.rank_mixed_exc(exc_list.ctypes, self).value
+        return libhci.rank_mixed_exc(exc_list.ctypes, self)
     
     def unrank_mixed_exc(self, exc_rank_ab: int) -> npt.NDArray[np.uintp]:
         r"""Determines the occupancy list corresponding to a mixed excitation with given rank.

hci/lib/hci_utils.py
@@ -21,7 +21,7 @@ def load_library(libname: str, loader_path: Path):
         print(f'Failed to load library with name {libname}.')
         raise
 
-libhci = load_library("libhci", Path(__file__).parent.parent.parent / 'build/hci/lib/')
+libhci = load_library("libhci", Path(__file__).parent)
 
 # def occslst2strs(occslst):
 #     occslst = np.asarray(occslst)